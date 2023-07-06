19c06187ef485bfb564525cf0a4d72a4ec35b357
deprussian <dmitrii.tsikh@gmail.com>
2023-07-06 20:39:43 +0500

    Update 4
    Added burger menu

--- javascript/date.js
+++ javascript/date.js
@@ -4,3 +4,10 @@ let today = new Date();
 let year = today.getFullYear();
 
 footerYearEl.textContent = year;
+
+const burger = document.querySelector('.menu__burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
